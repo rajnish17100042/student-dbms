45f04a67a58083d9ff32da8ae1798d87cd4ee603
Rajnish Patel <rajnish17100042@gmail.com>
2021-10-12 14:05:25 +0530

    protected admin and teacher registration routes using mounted lifecycle

--- client/src/components/admin/TeacherRegistration.vue
+++ client/src/components/admin/TeacherRegistration.vue
@@ -112,6 +112,39 @@
 <script>
 export default {
   name: "TeacherRegistration",
+  async mounted() {
+    try {
+      const response = await fetch(
+        "http://localhost:5000/admin/registrationAuthentication",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          credentials: "include" // very important to include ... authentication will not be done .... cookies will not be sent to the server  ...checked
+        }
+      );
+      const data = await response.json();
+
+      console.log(response);
+      console.log(data);
+
+      // check the status code sent from the backend
+      if (response.status !== 200 || !data) {
+        // return window.alert("Sorry! Something is missing");
+        // in case of any error redirect user to the login page
+        this.$router.push({ name: "Login" });
+      } else {
+        // display the data on the dashboard
+        console.log("rendering registration page");
+      }
+    } catch (err) {
+      // console.log(err);
+      // in case of any error redirect user to the login page
+      this.$router.push({ name: "Login" });
+    }
+  },
   data() {
     return {
       name: "",
