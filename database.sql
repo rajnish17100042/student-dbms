b856c5076bbdc6141711cc8f47e3d55e85a0206c
Rajnish Patel <86972671+rajnish17100042@users.noreply.github.com>
2021-10-25 13:25:41 +0530

    updated

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ CREATE TABLE `student-dbms`.`student_registration` ( `id` INT(255) NOT NULL AUTO
 
 
 -- table creation for teacher registration 
-CREATE TABLE `student-dbms`.`teacher_registration` ( `id` INT(255) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `phone` VARCHAR(13) NOT NULL , `address` TEXT NOT NULL , `city` VARCHAR(255) NOT NULL , `state` VARCHAR(255) NOT NULL , `pincode` VARCHAR(255) NOT NULL , `qualification` VARCHAR(255) NOT NULL , `experience` VARCHAR(255) NOT NULL , `joining_date` VARCHAR(11) NOT NULL , `salary` VARCHAR(11) NOT NULL , `password` VARCHAR(255) NOT NULL , `registered_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`), UNIQUE `email` (`email`)) ENGINE = InnoDB;
+CREATE TABLE `student-dbms`.`teacher_registration` ( `id` INT(255) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `phone` VARCHAR(13) NOT NULL , `address` TEXT NOT NULL , `city` VARCHAR(255) NOT NULL , `state` VARCHAR(255) NOT NULL , `pincode` VARCHAR(255) NOT NULL , `qualification` VARCHAR(255) NOT NULL , `experience` VARCHAR(255) NOT NULL , `joining_date` VARCHAR(255) NOT NULL , `salary` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `registered_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`), UNIQUE `email` (`email`)) ENGINE = InnoDB;
 
 
 -- table creation for admin registration 
@@ -12,4 +12,4 @@ CREATE TABLE `student-dbms`.`admin_registration` ( `id` INT(11) NOT NULL AUTO_IN
 
 -- table creation for notice 
 
-CREATE TABLE `student-dbms`.`notice_board` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(11) NOT NULL , `email` VARCHAR(11) NOT NULL , `group` VARCHAR(11) NOT NULL , `notice_date` VARCHAR(11) NOT NULL , `subject` VARCHAR(11) NOT NULL , `notice` TEXT NOT NULL , `issued_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `student-dbms`.`notice_board` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `group` VARCHAR(255) NOT NULL , `notice_date` VARCHAR(255) NOT NULL , `subject` VARCHAR(255) NOT NULL , `notice` TEXT NOT NULL , `issued_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
